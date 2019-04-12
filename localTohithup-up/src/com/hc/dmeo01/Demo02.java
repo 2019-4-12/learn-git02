9051eaf9ea1494a20bce6ad99aba4210413713d8
hc <9999@qq.com>
2019-04-12 17:11:56 +0800

    随便说点什么

--- localTohithup-up/src/com/hc/dmeo01/Demo02.java
+++ localTohithup-up/src/com/hc/dmeo01/Demo02.java
@@ -1,8 +1,20 @@
 package com.hc.dmeo01;
 
+/**
+ *  打印几行
+ * @author Administrator
+ *
+ */
 public class Demo02 {
 	
 	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ^ ");
+			}
+			System.out.println();
+		}
 		
 	}
 
