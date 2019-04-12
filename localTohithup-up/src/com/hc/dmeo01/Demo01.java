2dc1077b2a177961b8852d2b20f0f56aeddca2be
hc <9999@qq.com>
2019-04-12 16:48:46 +0800

    第一个,我靠半小时啊

---
+++ localTohithup-up/src/com/hc/dmeo01/Demo01.java
@@ -0,0 +1,9 @@
+package com.hc.dmeo01;
+
+public class Demo01 {
+	
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
---
+++ localTohithup-up/src/com/hc/dmeo01/Demo02.java
@@ -0,0 +1,9 @@
+package com.hc.dmeo01;
+
+public class Demo02 {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
